7a62a27267da65efd796f9baf0c2aae4e70d5b83
camiledias <camiledias@gmail.com>
2024-10-29 10:47:26 -0300

    img

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <h1>Camile Sacramento <h1>
+</head>
+
+<body>
+<img src="imagens/Pizza (1).jfif">
+png
+</body>
+</html>
